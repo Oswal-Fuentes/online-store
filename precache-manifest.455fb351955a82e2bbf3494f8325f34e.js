d5964a56ab50fa16c0b300704d0c024e8a148283
Oswal-Fuentes <oswal_f@unitec.edu>
2019-02-13 07:03:58 -0600

    Updates

--- precache-manifest.455fb351955a82e2bbf3494f8325f34e.js
+++ precache-manifest.455fb351955a82e2bbf3494f8325f34e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c7691980edf9b7aa146c",
+    "revision": "9f0b0cdd1dbf58643e85",
     "url": "/online-store/static/css/main.11822254.chunk.css"
   },
   {
-    "revision": "c7691980edf9b7aa146c",
-    "url": "/online-store/static/js/main.c7691980.chunk.js"
+    "revision": "9f0b0cdd1dbf58643e85",
+    "url": "/online-store/static/js/main.9f0b0cdd.chunk.js"
   },
   {
     "revision": "0ca3a5fa16c2f4b83e20",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/online-store/static/media/logo.5d5d9eef.svg"
   },
   {
-    "revision": "e44657037005ffbb69531a4484427b08",
+    "revision": "2521fc06c6563e40efeddfc7c284015b",
     "url": "/online-store/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/online-store/precache-manifest.995fd2e357d4b09d58a735c9f470770a.js"
+  "/online-store/precache-manifest.455fb351955a82e2bbf3494f8325f34e.js"
 );
 
 workbox.clientsClaim();
