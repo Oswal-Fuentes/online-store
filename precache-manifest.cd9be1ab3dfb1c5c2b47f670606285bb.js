99e1984ff9be570c8f5e8125939b6281cbc33270
Oswal-Fuentes <oswal_f@unitec.edu>
2019-02-12 18:23:59 -0600

    Updates

---
+++ precache-manifest.cd9be1ab3dfb1c5c2b47f670606285bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ae0908b751c3c69a320",
+    "url": "/online-store/static/css/main.11822254.chunk.css"
+  },
+  {
+    "revision": "8ae0908b751c3c69a320",
+    "url": "/online-store/static/js/main.8ae0908b.chunk.js"
+  },
+  {
+    "revision": "0ca3a5fa16c2f4b83e20",
+    "url": "/online-store/static/js/runtime~main.0ca3a5fa.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/online-store/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/online-store/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "52d908888e39328ad5353ba52139a7ef",
+    "url": "/online-store/index.html"
+  }
+];
