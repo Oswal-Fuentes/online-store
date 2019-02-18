ed648fd4112446d2014ea99a6d1f2a181c0b3d97
Oswal-Fuentes <oswal_f@unitec.edu>
2019-02-17 23:05:54 -0600

    Redux dependencies added

--- src/Cart.js
+++ src/Cart.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import Navbar from './Navbar';
+import Media from './Media'
+import { createSocket } from 'dgram';
+import ItemCard from './ItemCard'
 
 export default class Cart extends React.Component {
     constructor(props) {
         super(props);
-
     }
     render() {
         return (
             <div>
                 <Navbar />
-                Cart Page
             </div>
         );
     }
