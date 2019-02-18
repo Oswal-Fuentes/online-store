f3643bbe4cedcce52b58d91a6c2a2c05914a1caa
Oswal-Fuentes <oswal_f@unitec.edu>
2019-02-18 08:55:01 -0600

    unused imports removed

--- src/Cart.js
+++ src/Cart.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import Navbar from './Navbar';
-import Media from './Media'
 export default class Cart extends React.Component {
     constructor(props) {
         super(props);
